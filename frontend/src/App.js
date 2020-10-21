22ebdf088742a0df1d3c432b66d27feb85c52619
Alysson 'Inalvejavel' Aldrin <alysson_cdf@hotmail.com>
2020-10-21 17:11:44 -0300

    fetch working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+    const [candidates, setCandidates] = useState([]);
+
+    let interval = null;
+
+    useEffect(() => {
+        interval = setInterval(() => {
+            fetch("http://localhost:8080/votes")
+                .then(res => {
+                    return res.json()
+                }).then(json => {
+                    setCandidates(json.candidates);
+                })
+        }, 1000);
+    }, [])
+
+    if (candidates.length === 0) {
+        return (
+            <div className="preloader-wrapper big active">
+                <div className="spinner-layer spinner-blue-only">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
+                    </div><div className="gap-patch">
+                        <div className="circle"></div>
+                    </div><div className="circle-clipper right">
+                        <div className="circle"></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     return (
-        <span>Game of my life</span>
+        <div>
+            {candidates.map(({ id, name, votes }) => {
+                return (
+                    <p key={id}>
+                        {name} - {votes}
+                    </p>
+                );
+            })}
+        </div>
     )
 }
